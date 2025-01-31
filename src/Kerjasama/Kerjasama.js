3b620926324ba1901ad43975e17a6b54be98d40d
unknown <abdurrohimkholid04@gmail.com>
2025-01-31 22:51:21 +0700

    commit abdurrohim

---
+++ src/Kerjasama/Kerjasama.js
@@ -0,0 +1,15 @@
+import style from './Kerjasama.module.css'
+
+function Kerjasama() {
+
+    return (
+        <div>
+            <h1>Kerjasama</h1>
+        </div>
+
+    )
+}
+
+
+
+export default Kerjasama;
