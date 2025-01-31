231147124e2f9fe916b64b0a5e88092e612b9501
unknown <dhiyaulhaq80624@gmail.com>
2025-01-31 23:02:18 +0700

    commit ulhaq

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import body from './UnitPendidikan/Unitpendidikan';
 
 function App() {
   return (
---
+++ src/UnitPendidikan/Unitpendidikan.js
@@ -0,0 +1,11 @@
+import style from './Unitpendidikan.module.css'
+function Unitpendidikan() {
+    return (
+        <div>
+            <div>
+                <h1 className={style.Title}>UNIT PENDIDIKAN</h1>
+            </div>
+        </div>
+    );
+    
+}
