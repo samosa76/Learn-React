367e28d949aa9b4a1445e5c0279229567760754a
unknown <abdurrohimkholid04@gmail.com>
2025-01-31 23:25:47 +0700

    commit abdurrohim 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import body from './UnitPendidikan/Unitpendidikan';
-=======
 import Kerjasama from './Kerjasama/Kerjasama';
->>>>>>> 885b707b531423bb880b94303e7ed9880a3e56e4
-
 function App() {
   return (
     // <div className="App">
