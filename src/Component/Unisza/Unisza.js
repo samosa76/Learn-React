ad9849c46b000d286ba476c1cc0f9b48b8d51fd0
unknown <abdurrohimkholid04@gmail.com>
2025-02-01 18:14:54 +0700

    create kerjasama

--- src/Component/Unisza/Unisza.js
+++ src/Component/Unisza/Unisza.js
@@ -3,6 +3,14 @@ function Unisza() {
 
     return (
         <div className={style.container_Unisza}>
+            <div className={style.judul_kerjasama}>
+                <div className={style.title_kerjasama}>
+                   <h1> Kerjasama </h1>
+                </div>
+                <div className={style.desc_kerjasama}>
+                <p>Alhamdulillah, meskipun baru PPM IT Multimedia Al Fatih sudah melakukan kerjasama dengan 2 lembaga Internasional untuk alumni dan lulusannya. Kedua lembaga tersebut adalah :</p>
+                </div>
+            </div>
             <hr />
             <div className={style.content_Unisza}>
                 <div className={style.title_Unisza}>
@@ -16,9 +24,9 @@ function Unisza() {
                 </div>
             </div>
             <div className={style.image_Unisza}>
-                <img src="image/Unisza/kerjasama-1.jpeg"/> 
-                <img src="image/Unisza/kerjasama-2.jpeg"/> 
-                <img src="image/Unisza/kerjasama-3.jpeg"/> 
+                <img src="image/Unisza/kerjasama-1.jpeg" />
+                <img src="image/Unisza/kerjasama-2.jpeg" />
+                <img src="image/Unisza/kerjasama-3.jpeg" />
             </div>
         </div>
     )
