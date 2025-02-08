e8dd0eb39bda101725b9361ef080fcfbf7b9d164
unknown <muhammadmiftahusshiddiq@gmail.com>
2025-02-08 22:04:00 +0700

    create  Footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,7 +1,20 @@
+import style from './Footer.module.css'
+
 function Footer() {
     return (
-        <div>
-            
+        <div className={style.footer}>
+            <div className={style.text}>
+                <p>Pondok Pesantren Modern IT Multimedia Al Fatih</p>
+                <div className={style.img}>
+                    <div className={style.images}></div>
+                    <div className={style.images}></div>
+                    <div className={style.images}></div>
+                    <div className={style.images}></div>
+                </div>
+            </div>
+            <div className={style.name}>
+                <p>SCHOOL OF MULTIMEDIA</p>
+            </div>
         </div>
     )
 }
--- src/Pages/Pendaftaran.js
+++ src/Pages/Pendaftaran.js
@@ -1,11 +1,13 @@
 import ContentPendaftaran from "../Component/ContentPendaftaran/ContentPendaftaran";
+import Footer from "../Component/Footer/Footer";
 
 
 
 function Pendaftaran() {
     return (
         <div>
-           <ContentPendaftaran/>
+            <ContentPendaftaran />
+            <Footer />
         </div>
     );
 }
